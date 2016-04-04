b5640b9e50c8ef427159038d3ad1ab0d8692a900
DakotaH1ll <dakota.hill@uoit.net>
2016-04-04 13:40:42 -0400

    Vectors Implemented
    Justice Leage

--- projects/test/src/main.rs
+++ projects/test/src/main.rs
@@ -1,3 +1,18 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Dakota Hill's Testing Suite");
+    println!("Current Test: Vectors");
+    println!("---------------------------");
+
+    let mut vec = Vec::new();
+    vec.push("Bruce Wayne");
+    vec.push("Clark Kent");
+    vec.push("Diana Prince");
+    vec.push("Hal Jordan");
+    vec.push("Barry Allen");
+    vec.push("Arthur Curry");
+    vec.push("Victor Stone");
+
+    for x in &vec {
+    println!("{}", x);
+    }
 }
