d37efde25aafbd5d06b88f3f417fa5ece5f042a0
DakotaH1ll <dakota.hill@uoit.net>
2016-04-06 09:07:38 -0400

    Updated Vector Testing
    With Sorting, Printing etc.

--- projects/test/src/main.rs
+++ projects/test/src/main.rs
@@ -4,22 +4,39 @@ fn main() {
     println!("---------------------------");
 
     let mut vec = Vec::new();
+    vec.push("Cyborg");
+    vec.push("Wonder Woman");
     vec.push("Aquaman");
     vec.push("Batman");
-    vec.push("Cyborg");
-    vec.push("Flash");
     vec.push("Green Lantern");
+    vec.push("Flash");
     vec.push("Superman");
-    vec.push("Wonder Woman");
 
     let mut vec2 = Vec::new();
     vec2.push("Arthur Curry");
-    vec2.push("Barry Allen");
-    vec2.push("Bruce Wayne");
-    vec2.push("Clark Kent");
     vec2.push("Diana Prince");
-    vec2.push("Hal Jordan");
+    vec2.push("Clark Kent");
+    vec2.push("Bruce Wayne");
     vec2.push("Victor Stone");
+    vec2.push("Barry Allen");
+    vec2.push("Hal Jordan");
+
+
+    println!("{} is {}", vec2[0], vec[0]);
+    println!("{} is {}", vec2[1], vec[3]);
+    println!("{} is {}", vec2[2], vec[1]);
+    println!("{} is {}", vec2[3], vec[5]);
+    println!("{} is {}", vec2[4], vec[6]);
+    println!("{} is {}", vec2[5], vec[4]);
+    println!("{} is {}", vec2[6], vec[2]);
+    println!("");
+
+    println!("Wait a minute...");
+
+    vec.sort();
+    vec2.sort();
+    println!("");
+
 
 
 
