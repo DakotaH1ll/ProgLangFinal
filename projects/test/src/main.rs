1e039a2d5aee948df79954eb6f35ffd6359543d3
DakotaH1ll <dakota.hill@uoit.net>
2016-04-12 15:23:30 -0400

    Updated

--- projects/test/src/main.rs
+++ projects/test/src/main.rs
@@ -1,6 +1,6 @@
 fn main() {
     println!("Dakota Hill's Testing Suite");
-    println!("Current Test: Searching");
+    println!("Current Test: Studying For Calulus II Final");
     println!("---------------------------");
 
 }
