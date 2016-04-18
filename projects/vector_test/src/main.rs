466ad23b2d8b95b892029833e3d7a317c4ab3405
DakotaH1ll <dakota.hill@uoit.net>
2016-04-18 13:32:58 -0400

    Put Everything Together

--- projects/vector_test/src/main.rs
+++ projects/vector_test/src/main.rs
@@ -4,31 +4,27 @@ fn main() {
     println!("---------------------------");
 
     let mut vec = Vec::new();
-    vec.push("Cyborg");
-    vec.push("Wonder Woman");
-    vec.push("Aquaman");
-    vec.push("Batman");
-    vec.push("Green Lantern");
-    vec.push("Flash");
-    vec.push("Superman");
+    vec.push("Thor");
+    vec.push("Black Widow");
+    vec.push("Iron Man");
+    vec.push("Hawkeye");
+    vec.push("Captain America");
+    vec.push("Hulk");
 
     let mut vec2 = Vec::new();
-    vec2.push("Arthur Curry");
-    vec2.push("Diana Prince");
-    vec2.push("Clark Kent");
-    vec2.push("Bruce Wayne");
-    vec2.push("Victor Stone");
-    vec2.push("Barry Allen");
-    vec2.push("Hal Jordan");
-
+    vec2.push("Bruce Banner");
+    vec2.push("Clint Barton");
+    vec2.push("Steve Rogers");
+    vec2.push("Thor Odinson");
+    vec2.push("Tony Stark");
+    vec2.push("Natasha Romanoff");
 
     println!("{} is {}", vec2[0], vec[0]);
-    println!("{} is {}", vec2[1], vec[3]);
-    println!("{} is {}", vec2[2], vec[1]);
+    println!("{} is {}", vec2[1], vec[4]);
+    println!("{} is {}", vec2[2], vec[2]);
     println!("{} is {}", vec2[3], vec[5]);
-    println!("{} is {}", vec2[4], vec[6]);
-    println!("{} is {}", vec2[5], vec[4]);
-    println!("{} is {}", vec2[6], vec[2]);
+    println!("{} is {}", vec2[4], vec[1]);
+    println!("{} is {}", vec2[5], vec[3]);
     println!("");
 
     println!("Wait a minute...");
@@ -37,15 +33,11 @@ fn main() {
     vec2.sort();
     println!("");
 
-
-
-
-    println!("{} is {}", vec2[0], vec[0]);
-    println!("{} is {}", vec2[1], vec[3]);
-    println!("{} is {}", vec2[2], vec[1]);
-    println!("{} is {}", vec2[3], vec[5]);
-    println!("{} is {}", vec2[4], vec[6]);
+    println!("{} is {}", vec2[0], vec[3]);
+    println!("{} is {}", vec2[1], vec[2]);
+    println!("{} is {}", vec2[2], vec[0]);
+    println!("{} is {}", vec2[3], vec[1]);
+    println!("{} is {}", vec2[4], vec[5]);
     println!("{} is {}", vec2[5], vec[4]);
-    println!("{} is {}", vec2[6], vec[2]);
 
 }
